89f9f1ac700d4d7d4cf241417d845ccf65c803d9
Alejandra Bonilla <ale.bonilla309@gmail.com>
2024-01-22 09:52:04 +0100

    Prueba entornos

---
+++ Prueba5/src/CuentaBancoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaBancoTest {
+@Test
+public void testRetirar(){
+    CuentaBanco cuenta= new CuentaBanco(1500, "abc123");
+    int resultado= cuenta.retirar(500, "abc123");
+    assertEquals(1000,resultado);
+  }
+
+    @Test
+    public void testPasswordCorrecta(){
+    CuentaBanco cuenta= new CuentaBanco(1500, "abc123");
+    boolean resultado= cuenta.passwordCorrecta("abc123");
+    assertTrue(resultado);
+    }
+
+}
